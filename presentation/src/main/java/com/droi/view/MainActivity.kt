692fa64879e74504143757f1f75ffcc5e052c34e
jungkyu.yi <jumgkyu.yi@payhere.in>
2023-02-16 16:04:11 +0900

    g2

--- presentation/src/main/java/com/droi/view/MainActivity.kt
+++ presentation/src/main/java/com/droi/view/MainActivity.kt
@@ -21,8 +21,7 @@ class MainActivity : BaseActivity() {
 
     override fun _init() {
         initTap()
-        val f2 = "f2"
-        val f3 = "f3"
+        val g2 = "g2"
     }
 
     private fun initTap() {
