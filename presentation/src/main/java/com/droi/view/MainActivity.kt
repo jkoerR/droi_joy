df5c3fdbffe5fbdcd43c2c3d67c7b21d71c9929c
jungkyu.yi <jumgkyu.yi@payhere.in>
2023-02-16 16:45:45 +0900

    test1

--- presentation/src/main/java/com/droi/view/MainActivity.kt
+++ presentation/src/main/java/com/droi/view/MainActivity.kt
@@ -21,8 +21,7 @@ class MainActivity : BaseActivity() {
 
     override fun _init() {
         initTap()
-        val test2 = "test2"
-        val test4 = "test4"
+        val test1= "test1"
     }
 
     private fun initTap() {
